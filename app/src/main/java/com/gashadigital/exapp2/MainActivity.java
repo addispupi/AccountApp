05b0d4841ddba1093b571545f80dae27a9d4820f
Addisu <pupiatpixar@gmail.com>
2021-06-23 19:28:25 +0300

    MySql Register - Updated

--- app/src/main/java/com/gashadigital/exapp2/MainActivity.java
+++ app/src/main/java/com/gashadigital/exapp2/MainActivity.java
@@ -6,28 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText emailText;
+    private EditText passText;
+    private TextView attemptInfo;
+    private Button btnLogin;
 
-    public void clickLogin(View view){
-        EditText emailText = (EditText) findViewById(R.id.email);
-        EditText passText = (EditText) findViewById(R.id.pass);
-        String userEmail = "addis@haile.com";
-        String userPass = "new";
+    private String userEmail = "addis";
+    private String userPass = "new";
+    private String userName = "Addis";
 
+    boolean isValid = false;
+    private int passCounter = 5;
 
-        if(emailText.getText().toString().equals(userEmail) && passText.getText().toString().equals(userPass)){
-            Log.i("Info: ", "Successful");
-            Log.i("email: ", emailText.getText().toString());
-            Log.i("pass: ",passText.getText().toString());
-        }
-        else {
-            Toast.makeText(this, "Username or Password not Correct", Toast.LENGTH_LONG).show();
-        }
-
-    }
 
     public void userRegister(View view) {
         startActivity(new Intent(this, Register.class));
@@ -38,6 +34,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       emailText = findViewById(R.id.email);
+       passText = findViewById(R.id.pass);
+       attemptInfo = findViewById(R.id.tvAttemptInfo);
+       btnLogin = findViewById(R.id.btnLogin);
 
+       btnLogin.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               String getUser = emailText.getText().toString();
+               String getPass = passText.getText().toString();
+               String getAttempt = getString(R.string.attempt);
+
+
+               if(getUser.equals("") || getPass.equals("")){
+                   Toast.makeText(MainActivity.this, "Please provide info. All fields must be filled", Toast.LENGTH_SHORT).show();
+               }
+               else {
+                   isValid = validate(getUser, getPass);
+                   if(!isValid){
+                       passCounter--;
+                       Toast.makeText(MainActivity.this, "Username or Password not Correct !", Toast.LENGTH_SHORT).show();
+                       attemptInfo.setText(getAttempt + " " +passCounter);
+                       attemptInfo.setVisibility(View.VISIBLE);
+                       if(passCounter <= 0){
+                           btnLogin.setEnabled(false);
+                           Log.i("Info: ","Disable the Button");
+                       }
+                   }
+                   else{
+                       Log.i("Info", "Successful");
+                       Log.i("email", getUser);
+                       Log.i("pass",getPass);
+                       Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                       startActivity(new Intent(MainActivity.this, HomePage.class));
+                   }
+               }
+           }
+       });
+
+    }
+
+    private boolean validate(String name, String password){
+        if(name.equals(userEmail) && password.equals(userPass)){
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/gashadigital/exapp2/Register.java
+++ app/src/main/java/com/gashadigital/exapp2/Register.java
@@ -3,14 +3,19 @@ package com.gashadigital.exapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends Activity {
 
-    EditText userRegFName, userRegLName, userRegEmail, userRegPass;
-    String userF_name, userL_name, user_email, user_pass;
+    EditText userRegFName, userRegLName, userRegEmail, userRegPass, userRegConfirm;
+    String userF_name, userL_name, user_email, user_pass, user_confirm;
+    Button btnRegister;
+    private boolean isValidate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +26,41 @@ public class Register extends Activity {
         userRegLName = (EditText) findViewById(R.id.lname);
         userRegEmail = (EditText) findViewById(R.id.userEmail);
         userRegPass = (EditText) findViewById(R.id.newPass);
+        userRegConfirm = findViewById(R.id.confirmPass);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userF_name = userRegFName.getText().toString();
+                userL_name = userRegLName.getText().toString();
+                user_email = userRegEmail.getText().toString();
+                user_pass = userRegPass.getText().toString();
+                user_confirm = userRegConfirm.getText().toString();
+
+                isValidate = validate(userF_name, userL_name, user_email, user_pass);
+
+                if(isValidate) {
+                    String method = "Register";
+                    ServiceTask serviceTask = new ServiceTask(Register.this);
+                    serviceTask.execute(method, userF_name, userL_name, user_email, user_pass);
+
+                }
+            }
+        });
+
     }
-    public void uerReg(View view){
-        userF_name = userRegFName.getText().toString();
-        userL_name = userRegLName.getText().toString();
-        user_email = userRegEmail.getText().toString();
-        user_pass = userRegPass.getText().toString();
-        String method = "Register";
-        ServiceTask serviceTask = new ServiceTask(this);
-        serviceTask.execute(method, userF_name, userL_name, user_email, user_pass);
-        //finish();
+
+    private boolean validate(String rFName, String rLName, String rEmail, String rPass){
+        if(rFName.equals("") || rLName.equals("") || rEmail.equals("") || rPass.equals("")){
+            Toast.makeText(this, "All the fields can not be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(!rPass.equals(user_confirm)){
+            Toast.makeText(this, "Both password fields should be the same", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
+
 }
